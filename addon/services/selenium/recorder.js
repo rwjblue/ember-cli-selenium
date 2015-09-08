280b6b53bab03c7924a4f329a86a336b47c1f6c6
Robert Jackson <robert.w.jackson@me.com>
2015-09-07 22:55:25 -0400

    Add ability to grab a time.

--- addon/services/selenium/recorder.js
+++ addon/services/selenium/recorder.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  performance: performance, // jshint ignore:line
+  Date: Date, // jshint ignore:line
 
+  getTime() {
+    if (this.performance && this.performance.now) {
+      return this.performance.now();
+    } else {
+      return this.Date.now();
+    }
+  }
 });
--- tests/unit/services/selenium/recorder-test.js
+++ tests/unit/services/selenium/recorder-test.js
@@ -4,8 +4,34 @@ moduleFor('service:selenium/recorder', {
 
 });
 
-test('exists', function(assert) {
-  let service = this.subject();
+test('can get a value when performance is available', function(assert) {
+  let service = this.subject({
+    performance: {
+      now() {
+        assert.ok(true, 'this.performance.now was called');
+        return 123456789;
+      }
+    }
+  });
 
-  assert.ok(!!service, 'service exists and can be looked up from app/');
+  let result = service.getTime();
+
+  assert.equal(result, 123456789, 'result of performance.now() is returned');
+});
+
+test('can get a value when performance is not available', function(assert) {
+  let service = this.subject({
+    performance: null,
+
+    Date: {
+      now() {
+        assert.ok(true, 'this.Date.now was called');
+        return 123456789;
+      }
+    }
+  });
+
+  let result = service.getTime();
+
+  assert.equal(result, 123456789, 'result of Date.now() is returned');
 });
