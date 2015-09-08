f7765921f5b570169c1ae74268583ca1a1f85f0e
Robert Jackson <robert.w.jackson@me.com>
2015-09-08 00:05:51 -0400

    Continue fleshing out `recorder` service.

--- addon/services/selenium/recorder.js
+++ addon/services/selenium/recorder.js
@@ -4,11 +4,41 @@ export default Ember.Service.extend({
   performance: performance, // jshint ignore:line
   Date: Date, // jshint ignore:line
 
+  init() {
+    this._super(...arguments);
+
+    this._timers = {};
+  },
+
   getTime() {
     if (this.performance && this.performance.now) {
       return this.performance.now();
     } else {
       return this.Date.now();
     }
+  },
+
+  startTimer(label) {
+    return this._timers[label] = { start: this.getTime(), end: null };
+  },
+
+  stopTimer(label) {
+    if (!this._timers[label]) {
+      throw new Error('You called `stopTimer` without having called `startTimer` first.');
+    }
+
+    this._timers[label].end = this.getTime();
+
+    return this._timers[label];
+  },
+
+  timedRender(label) {
+    let result = this.startTimer(label);
+
+    Ember.run.schedule('afterRender', () => {
+      this.stopTimer(label);
+    });
+
+    return result;
   }
 });
--- tests/unit/services/selenium/recorder-test.js
+++ tests/unit/services/selenium/recorder-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:selenium/recorder', {
@@ -34,4 +35,90 @@ test('can get a value when performance is not available', function(assert) {
   let result = service.getTime();
 
   assert.equal(result, 123456789, 'result of Date.now() is returned');
+});
+
+test('startTimer can be invoked', function(assert) {
+  let service = this.subject({
+    performance: {
+      now() {
+        assert.ok(true, 'this.performance.now was called');
+        return 123456789;
+      }
+    }
+  });
+
+  assert.ok(service.startTimer, 'startTimer function exists');
+
+  let result = service.startTimer('foo');
+  let expected = { start: 123456789, end: null };
+
+  assert.deepEqual(result, expected, 'object is returned');
+});
+
+test('stopTimer throws an error if startTimer was not invoked first', function(assert) {
+  let service = this.subject();
+
+  assert.throws(() => {
+    service.stopTimer('foo');
+  }, /You called `stopTimer` without having called `startTimer` first\./);
+});
+
+test('stopTimer can be invoked', function(assert) {
+  let times = [1000, 2000];
+  let timeIndex = -1;
+
+  let service = this.subject({
+    performance: {
+      now() {
+        timeIndex++;
+
+        return times[timeIndex];
+      }
+    }
+  });
+
+  assert.ok(service.startTimer, 'startTimer function exists');
+
+  service.startTimer('foo');
+  let result = service.stopTimer('foo');
+
+  let expected = { start: 1000, end: 2000 };
+
+  assert.deepEqual(result, expected, 'object is returned');
+});
+
+test('timedRender starts a timer and schedules ending a timer in the afterRender queue', function(assert) {
+  let timeToReturn = 1000;
+  let result;
+
+  let service = this.subject({
+    getTime() {
+      assert.ok(true, 'getTime was called');
+
+      return timeToReturn;
+    }
+  });
+
+  // start a run loop manually
+  Ember.run.begin();
+
+  result = service.timedRender('foo');
+
+  assert.deepEqual(result, { start: 1000, end: null }, 'initial result is correct');
+
+  // change the time to be returned by the next call to `getTime`
+  timeToReturn = 2000;
+
+  Ember.run.schedule('afterRender', () => {
+    // this is to ensure that the timedRender scheduled a
+    // getTime in afterRender, and it should be called *before*
+    // this afterRender callback is invoked
+    timeToReturn = 3000;
+  });
+
+  Ember.run.end();
+
+  assert.deepEqual(result, { start: 1000, end: 2000 }, 'result is correct afterRender is complete');
+
+  assert.equal(service.getTime(), 3000, 'confirm the little smoke and mirrors above actual did something...');
 });
